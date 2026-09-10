ed30d5c74db48fcf80343c086a55b06b3f6bf180
As enterprises <asenterprises1929@gmail.com>
2026-09-10 06:06:10 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -207,9 +207,7 @@ const [items, setItems] = useState([]);
     } catch (e) {
       console.error("Error saving item:", e);
     }
-  };
-
-
+  }}
   const cartTotal = useMemo(() => cart.reduce((s, x) => s + x.p * x.q, 0), [cart]);
 
   const filtered = useMemo(() => {
